20eb5cd4355347aa2281f88019f5d0f62568aa81
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-03 17:30:29 +0300

    Added Express to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Here");
+const express = require("express");
+
+const app = express();
+
+const PORT = 5050;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
