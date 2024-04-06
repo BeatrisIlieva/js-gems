8fff7dbba192b7c0eb00aad0e6156294efe192fa
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-06 13:19:13 +0300

    Added register page

--- src/controllers/shoppingBagController.js
+++ src/controllers/shoppingBagController.js
@@ -3,7 +3,6 @@ const router = require("express").Router();
 const earringManager = require("../managers/earringManager");
 
 router.get("/shopping-bag", (req, res) => {
-  console.log(earringManager.getAll());
   res.redirect("shopping-bag");
 });
 
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController");
 const earringController = require("./controllers/earringController");
+const userController = require("./controllers/userController");
 
 router.use(homeController);
 router.use("/earrings", earringController);
+router.use("/users", userController);
 router.get("*", (req, res) => {
   res.redirect("/404");
 });
