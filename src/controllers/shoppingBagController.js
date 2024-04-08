fbe487f6943d8d02e9290c36945b3425e6ae926d
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-08 19:03:24 +0300

    Added views

--- src/controllers/shoppingBagController.js
+++ src/controllers/shoppingBagController.js
@@ -1,17 +1,18 @@
 const router = require("express").Router();
-const {isAuth} = require("../middlewares/authMiddleware");
-const {DEFAULT_ADD_QUANTITY, DEFAULT_MIN_QUANTITY} = require("../constants/shoppingBag");
+const { isAuth } = require("../middlewares/authMiddleware");
+const {
+  DEFAULT_ADD_QUANTITY,
+  DEFAULT_MIN_QUANTITY,
+} = require("../constants/shoppingBag");
 
 const shoppingBagManager = require("../managers/shoppingBagManager");
-const ShoppingBag = require("../models/ShoppingBag");
-const Jewelry = require("../models/Jewelry");
 
 router.get("/:userId", isAuth, async (req, res) => {
   const userId = req.params.userId;
-  
+
   const jewelries = await shoppingBagManager.getAll(userId);
 
-  res.render("bag/display", {jewelries, DEFAULT_MIN_QUANTITY});
+  res.render("bag/display", { jewelries, DEFAULT_MIN_QUANTITY });
 });
 
 router.post("/:jewelryId", isAuth, async (req, res) => {
@@ -19,30 +20,33 @@ router.post("/:jewelryId", isAuth, async (req, res) => {
 
   const jewelryId = req.params.jewelryId;
 
-  const {size} = req.body;
+  const { size } = req.body;
 
   await shoppingBagManager.createOrUpdate({
     userId,
     jewelryId,
     sizeId: size,
     quantity: DEFAULT_ADD_QUANTITY,
-  })
+  });
 
   res.redirect(`/shopping-bag/${userId}`);
 });
 
-router.post("/:jewelryId/:sizeId/update", async (req, res) => {
+router.post("/:jewelryId/:sizeId/update", isAuth, async (req, res) => {
   const userId = req.user._id;
   const jewelryId = req.params.jewelryId;
   const sizeId = req.params.sizeId;
 
-  const {updatedQuantity} = req.body;
+  const { updatedQuantity } = req.body;
 
-  await shoppingBagManager.updateQuantity(userId, jewelryId, sizeId, updatedQuantity);
+  await shoppingBagManager.updateQuantity(
+    userId,
+    jewelryId,
+    sizeId,
+    updatedQuantity
+  );
 
   res.redirect(`/shopping-bag/${userId}`);
-})
+});
 
 module.exports = router;
-
-
