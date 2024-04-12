c4effe4b56717e9ac6cf4bc6c30895e1f923472b
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-12 15:24:26 +0300

    Added CSS

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,25 @@ const userManager = require("../managers/userManager");
 const { extractErrorMessages } = require("../utils/errorHelpers");
 const {TOKEN_KEY} = require("../config/config");
 
+// router.get("/login-or-register", (req, res) => {
+//   res.render("users/loginOrRegister");
+// });
+
+// router.post("/login-or-register", async (req, res, next) => {
+//   const { email, password } = req.body;
+  
+//   try {
+//     const token = await userManager.login(email, password);
+
+//     res.cookie(TOKEN_KEY, token, { httpOnly: true });
+
+//     res.redirect("/");
+//   } catch (err) {
+//       const errorMessages = extractErrorMessages(err);
+//       res.status(404).render("users/login", { errorMessages });
+//   }
+// });
+
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
