6fb26472a1494f10414a00542be69c11c4057abf
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-16 18:53:06 +0300

    Added validation

--- src/managers/wishlistManager.js
+++ src/managers/wishlistManager.js
@@ -23,37 +23,8 @@ exports.isLikedByUser = async ({ userId, jewelryId }) => {
 };
 
 exports.getAll = async (userId) => {
-  const jewelries = await Wishlist.find({user: userId}).populate("jewelry").lean();
-  console.log(jewelries);
+  const jewelries = await Wishlist.find({ user: userId })
+    .populate("jewelry")
+    .lean();
   return jewelries;
-
-  // for (let i = 0; i < result.length; i++) {
-  //     const jewelryId = result[i].jewelry;
-  //     const wishlistId = result[i]._id;
-
-  //     const jewelry = await Jewelry.findById(jewelryId)
-  //       .populate("category")
-  //       .populate("metals.kind")
-  //       .populate("metals.caratWeight")
-  //       .populate("stones.kind")
-  //       .populate("stones.color")
-  //       .populate("stones.caratWeight")
-  //       .lean();
-  //     bagItems[bagItemId] = {
-  //       bagItemId,
-  //       jewelry: jewelry,
-  //       size: size,
-  //       quantity: quantity,
-  //       totalPrice: totalPrice,
-  //       maxQuantity: maxQuantity,
-  //       minQuantity: DEFAULT_MIN_QUANTITY,
-  //     };
-  //     subTotal += totalPrice;
-  //   }
 };
-
-// async function isLikedByUser({ userId, jewelryId }) {
-//     return await Wishlist.findOne({ userId, jewelryId }).exists;
-// };
-
-
