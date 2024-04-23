ae6f9fb404da6e2c66a7d3f3694ce2a0c14bfcd5
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-23 14:45:46 +0300

    Modified storeOriginalUrlMiddleware

--- src/middlewares/storeOriginalUrlMiddleware.js
+++ src/middlewares/storeOriginalUrlMiddleware.js
@@ -2,7 +2,8 @@ exports.storeOriginalUrl = (req, res, next) => {
   if (
     req.url !== "/users/register" &&
     req.url !== "/users/login" &&
-    req.url !== "/css/icons.css"
+    req.url !== "/css/icons.css" &&
+    req.url !== "/users/logout"
   ) {
     req.session.originalUrl = req.originalUrl || req.url;
   }
