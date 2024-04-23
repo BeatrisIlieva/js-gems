6f0dc10141bb4acb43094f331533325f2c01f40a
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-23 17:55:05 +0300

    Updated wishlistManager

--- src/managers/wishlistAuthUserManager.js
+++ src/managers/wishlistAuthUserManager.js
@@ -1,10 +1,7 @@
 const Wishlist = require("../models/Wishlist");
 const Jewelry = require("../models/Jewelry");
+const User = require("../models/User");
 const { setJewelriesLikedAuthUser } = require("../utils/setIsLikedAuthUser");
-const {
-  isSelectionEmpty,
-  isArrayEmpty,
-} = require("../utils/checkIfCollectionIsEmpty");
 
 exports.create = async (userId, jewelryId) => {
   wishlist = await Wishlist.create({
@@ -21,10 +18,88 @@ exports.delete = async (userId, jewelryId) => {
 };
 
 exports.getAll = async (userId) => {
-  let jewelryIds = await Wishlist.find({ user: userId }).select("jewelry");
-  jewelryIds = jewelryIds.map((item) => item.jewelry);
+  const user = await User.findById(userId);
 
-  let jewelries = await Jewelry.find({ _id: { $in: jewelryIds } }).lean();
+  let jewelries = await Jewelry.aggregate([
+    {
+      $lookup: {
+        as: "wishlists",
+        from: "wishlists",
+        foreignField: "jewelry",
+        localField: "_id",
+      },
+    },
+    {
+      $match: {
+        "wishlists.user": user._id,
+      },
+    },
+    {
+      $lookup: {
+        as: "inventories",
+        from: "inventories",
+        foreignField: "jewelry",
+        localField: "_id",
+      },
+    },
+    {
+      $lookup: {
+        as: "categories",
+        from: "categories",
+        foreignField: "_id",
+        localField: "category",
+      },
+    },
+    {
+      $match: {
+        "inventories.quantity": {
+          $gt: 0,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        price: {
+          $first: {
+            $arrayElemAt: ["$inventories.price", 0],
+          },
+        },
+        firstImageUrl: {
+          $addToSet: "$firstImageUrl",
+        },
+        jewelryIds: {
+          $push: "$_id",
+        },
+        categoryTitle: {
+          $addToSet: "$categories.title",
+        },
+        jewelryTitle: {
+          $addToSet: "$title",
+        },
+        createdAt: {
+          $first: {
+            $arrayElemAt: ["$wishlists.createdAt", 0],
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        price: 1,
+        firstImageUrl: 1,
+        jewelryIds: 1,
+        categoryTitle: 1,
+        jewelryTitle: 1,
+        createdAt: 1,
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+  ]);
 
   jewelries = await setJewelriesLikedAuthUser(jewelries, userId);
 
