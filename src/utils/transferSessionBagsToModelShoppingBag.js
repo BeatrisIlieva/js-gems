abf8c8918903b19b03dbf1b001c159d502b6f670
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-23 13:07:17 +0300

    Updated transferSessionBagsToModelShoppingBag

--- src/utils/transferSessionBagsToModelShoppingBag.js
+++ src/utils/transferSessionBagsToModelShoppingBag.js
@@ -1,7 +1,8 @@
 const ShoppingBag = require("../models/ShoppingBag");
 
 exports.transferSessionBagsToModelShoppingBag = async (sessionId, userId) => {
-  
- await ShoppingBag.updateMany({session: sessionId, user: null}, { $set: { user: userId } });
-
+  await ShoppingBag.updateMany(
+    { session: sessionId, user: null },
+    { $set: { user: userId } }
+  );
 };
