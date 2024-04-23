86175c9f2236f76c2d1e3c178d1857b8045a30f4
Beatris Ilieve <beatrisilieve@icloud.com>
2024-04-23 10:22:04 +0300

    Updated orders history

--- src/controllers/orderHistoryController.js
+++ src/controllers/orderHistoryController.js
@@ -8,12 +8,6 @@ router.get("/:userId", isAuth, getBagCount, getLikeCount, async (req, res) => {
     const userId = req.user._id;
     try {
         orderItems = await Order.find({user: userId}).lean();
-        orderItems.forEach(element => {
-            console.log(element.jewelries);
-            console.log(element._id);
-        });
-
-
         res.render("orders/ordersHistory", {orderItems});
       } catch (err) {
         console.log(err.message);
